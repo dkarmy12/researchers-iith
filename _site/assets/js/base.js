bd91e7e105eca9b57256c158ad66e589b47dc32d
dkarmy12 <55491427+dkarmy12@users.noreply.github.com>
2021-04-28 23:12:32 +0530

    Final

--- _site/assets/js/base.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    let elems = document.querySelectorAll('.sidenav');
-    let instances = M.Sidenav.init(elems);
-    elems = document.querySelectorAll('.dropdown-trigger');
-    instances = M.Dropdown.init(elems);
-  });
--- _site/assets/js/index.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.parallax');
-    var instances = M.Parallax.init(elems);
-    // elems = document.querySelectorAll('.collapsible');
-    // instances = M.Collapsible.init(elems);
-    // elems = document.querySelectorAll('.tabs')
-    // var instance = M.Tabs.init(elems, {swipeable:true});
-});
