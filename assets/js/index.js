ca031602d793fe878fa1b7fb2bfe65f11aa0767b
dkarmy12 <55491427+dkarmy12@users.noreply.github.com>
2021-05-23 17:33:33 +0530

    Cleaned up the code

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,26 +1,26 @@
-document.addEventListener('DOMContentLoaded', function() {
-    let elems = document.querySelectorAll('.carousel');
-    let instances = M.Carousel.init(elems, {
-        fullWidth: true,
-        indicators:true,
-        dist:0,
-        duration:500
-    });
-// custom function for autoplaying
-    let indicatorItems = document.querySelectorAll('.carousel .indicator-item');
-    let slideTime = 4000;
-    let activeClass = "active";
-
-setInterval(() => {
-  indicatorItems.forEach(el => {
-    if (el.classList.contains(activeClass)) {
-      sib = el.nextElementSibling;
-      if (sib == null) {
-        indicatorItems[0].click();
-      } else {
-        sib.click()
-      }
-    }
+document.addEventListener("DOMContentLoaded", function () {
+  let elems = document.querySelectorAll(".carousel");
+  let instances = M.Carousel.init(elems, {
+    fullWidth: true,
+    indicators: true,
+    dist: 0,
+    duration: 500,
   });
-}, slideTime);
+  // custom function for autoplaying
+  let indicatorItems = document.querySelectorAll(".carousel .indicator-item");
+  let slideTime = 4000;
+  let activeClass = "active";
+
+  setInterval(() => {
+    indicatorItems.forEach((el) => {
+      if (el.classList.contains(activeClass)) {
+        sib = el.nextElementSibling;
+        if (sib == null) {
+          indicatorItems[0].click();
+        } else {
+          sib.click();
+        }
+      }
+    });
+  }, slideTime);
 });
